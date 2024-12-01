857fc9533b341674c03974882f2a12383102be08
Keks <keks@htmlacademy.ru>
2024-12-01 18:56:03 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-unused-vars
+import { getPhoto } from './data.js';
+
+// Генерация 25 фотографий
+/*
+const photoes = Array.from({length: 25}, getPhoto());
+console.log(photoes);
+*/
+
+/*
+ОПИСАНИЕ
+Раздление на модули
+main.js - точка входа
+
+utils.js - модуль свспомогательными фукнцияи
+
+data.js - модуль с получением(генерацией) фотографий
+
+comments.js - модуль для получения(генерации) комментариев к фотографии
+
+miniatures.js - модуль по отрисовке миниатюр
+*/
