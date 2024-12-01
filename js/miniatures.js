59e213587c304b276428c8e0b0eb300962a54c6f
MaT0kz <mixail.gerasimov.1994@gmail.com>
2024-12-01 20:56:36 +0500

    added miniatures

--- js/miniatures.js
+++ js/miniatures.js
@@ -1,2 +1,18 @@
-let pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
-console.log(pictureTemplate);
+import { getPhoto } from './data.js';
+
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const picturesContainer = document.querySelector('.pictures');
+const pictures = Array.from({length: 25}, getPhoto());
+const fragment = document.createDocumentFragment();
+
+pictures.forEach(({url, description, likes, comments}) => {
+  const newPicture = pictureTemplate.cloneNode(true);
+  const img = newPicture.querySelector('.picture__img');
+  img.src = url;
+  img.alt = description;
+  newPicture.querySelector('.picture__likes').textContent = likes;
+  newPicture.querySelector('.picture__comments').textContent = comments.length;
+  fragment.append(newPicture);
+});
+
+picturesContainer.append(fragment);
