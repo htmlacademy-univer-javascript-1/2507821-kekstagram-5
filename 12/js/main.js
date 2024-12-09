7f2bb6c9c005af5c5f56e2bf834b41e9f2c24d6e
Keks <keks@htmlacademy.ru>
2024-12-09 12:44:04 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/main.js
@@ -0,0 +1,26 @@
+import { getPhoto } from './data.js';
+
+// Генерация 25 фотографий
+/*
+const photoes = Array.from({length: 25}, getPhoto());
+console.log(photoes);
+*/
+
+/*
+ОПИСАНИЕ
+Раздление на модули
+main.js - точка входа
+
+utils.js - модуль свспомогательными фукнцияи
+
+data.js - модуль с получением(генерацией) фотографий
+
+comments.js - модуль для получения(генерации) комментариев к фотографии
+
+miniatures.js - модуль по отрисовке миниатюр
+*/
+
+
+const photoes = Array.from({length: 25}, getPhoto());
+
+export default photoes;
