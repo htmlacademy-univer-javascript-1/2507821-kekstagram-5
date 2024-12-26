5922a4cb8a3ad28510e3160ec9c4d9aa82949790
MaT0kz <mixail.gerasimov.1994@gmail.com>
2024-12-27 01:18:36 +0500

    Валидация хэш-тегов(кастомная через Pristine) + сообщение о ошибке загрузки миниатюр + код расфасован по файлам

--- js/main.js
+++ js/main.js
@@ -1,53 +1,12 @@
 import { downloadImages } from './requests.js';
-import { showPhotos } from './miniatures.js';
-import { debounce, getRandomInteger } from './utils.js';
+import { photoSetter } from './utils.js';
 
-const filterButtons = document.querySelectorAll('.img-filters button');
-let currentFilter = 'default';
-const filterList = {
-  'filter-default': 'default',
-  'filter-random': 'random',
-  'filter-discussed': 'discussed'
+const body = document.body;
+
+const main = async () => {
+  await downloadImages(photoSetter);
 };
 
-const photos = await downloadImages();
+main();
 
-const filterRandomly = (array, picturesNumber = 10, upperBorder = 24) => {
-  const previousIds = [];
-  for (let i = 0; i < picturesNumber; i++) {
-    let currentValue = getRandomInteger(1, upperBorder);
-    while (previousIds.includes(currentValue)) {
-      currentValue = getRandomInteger(1, upperBorder);
-    }
-    previousIds.push(currentValue);
-  }
-  return array.filter((element) => previousIds.includes(element.id));
-};
-
-const getFilteredPhotos = (photosToFilter, filter = 'none') => {
-  if (filter === 'discussed') {
-    return photosToFilter.slice().sort((a, b) => b.comments.length - a.comments.length);
-  } else if (filter === 'random') {
-    return filterRandomly(photosToFilter);
-  }
-  return photosToFilter;
-};
-
-const debouncedShowPhotos = debounce(() => showPhotos(getFilteredPhotos(photos, currentFilter)), 500);
-
-filterButtons.forEach((button) => {
-  button.addEventListener('click', (evt) => {
-    filterButtons.forEach((btn) => (btn === evt.target) ? btn.classList.add('img-filters__button--active') : btn.classList.remove('img-filters__button--active'));
-    currentFilter = filterList[evt.target.id];
-    debouncedShowPhotos();
-  });
-});
-
-
-if (photos) {
-  document.querySelector('.img-filters').classList.remove('img-filters--inactive');
-}
-showPhotos(getFilteredPhotos(photos, currentFilter));
-
-
-export default photos;
+export {body};
--- js/requests.js
+++ js/requests.js
@@ -1,6 +1,13 @@
+import { downloadErrorMessage } from './messages.js';
+import { addFilterFunctions, debouncedShowPhotos } from './utils.js';
+
+const url = 'https://29.javascript.htmlacademy.pro/kekstagram/data';
+
 const sendImage = async (data) => {
   data.submitter.disabled = true;
   const requestData = new FormData(data.form);
+  requestData.set('hashtags', requestData.get('hashtags').trim().split(/\s+/).join(' '));
+
   fetch('https://29.javascript.htmlacademy.pro/kekstagram', {
     method: 'post',
     body: requestData
@@ -16,10 +23,17 @@ const sendImage = async (data) => {
   });
 };
 
-const downloadImages = async () => {
-  const response = await fetch('https://29.javascript.htmlacademy.pro/kekstagram/data');
-  const data = await response.json();
-  return data;
+const downloadImages = async (setter) => {
+  const response = await fetch(url);
+  if (response.ok) {
+    const data = await response.json();
+    setter(data);
+    downloadErrorMessage.closeMessage();
+    addFilterFunctions(debouncedShowPhotos);
+    return;
+  }
+  downloadErrorMessage.showDownloadError(downloadImages, setter);
+  downloadErrorMessage.setDownloadErrorShowen(true);
 };
 
 export {sendImage, downloadImages};
